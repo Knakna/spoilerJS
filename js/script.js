0f79e1dd4cb5714da7fb81f82ae85d25254b5f2d
knakna <kna@tut.by>
2021-10-14 18:00:17 +0300

    add img how bg

--- js/script.js
+++ js/script.js
@@ -462,3 +462,17 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+
+//==================================================================================================================
+// img  используем как bg
+
+
+
+function ibg(){
+	$.each($('.ibg'), function(index, val) {
+		if($(this).find('img').length>0){
+			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+		}
+	});
+}
+ibg();
